8a62ad6ee0b05aedc2440b55ff2feaa97dc95ba0
Kenny Pflug <kenny.pflug@live.de>
2022-09-27 11:53:53 +0200

    started implementation of ProcessBuilder

---
+++ Code/Synnotech.FluentProcesses/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace Synnotech.FluentProcesses;
+
+public static class Extensions
+{
+    public static ProcessStartInfo Clone(this ProcessStartInfo processStartInfo, bool copyEnvironment = false)
+    {
+        var clone = new ProcessStartInfo
+        {
+            Arguments = processStartInfo.Arguments,
+            Domain = processStartInfo.Domain,
+            CreateNoWindow = processStartInfo.CreateNoWindow,
+            Password = processStartInfo.Password,
+            Verb = processStartInfo.Verb,
+            ErrorDialog = processStartInfo.ErrorDialog,
+            FileName = processStartInfo.FileName,
+            UserName = processStartInfo.UserName,
+            WindowStyle = processStartInfo.WindowStyle,
+            WorkingDirectory = processStartInfo.WorkingDirectory,
+            LoadUserProfile = processStartInfo.LoadUserProfile,
+            RedirectStandardError = processStartInfo.RedirectStandardError,
+            RedirectStandardInput = processStartInfo.RedirectStandardInput,
+            RedirectStandardOutput = processStartInfo.RedirectStandardOutput,
+            StandardErrorEncoding = processStartInfo.StandardErrorEncoding,
+            StandardOutputEncoding = processStartInfo.StandardOutputEncoding,
+            UseShellExecute = processStartInfo.UseShellExecute,
+            ErrorDialogParentHandle = processStartInfo.ErrorDialogParentHandle,
+            PasswordInClearText = processStartInfo.PasswordInClearText
+        };
+
+        if (!copyEnvironment)
+            return clone;
+        
+        foreach (var keyValuePair in processStartInfo.Environment)
+        {
+            clone.Environment.Add(keyValuePair);
+        }
+
+        return clone;
+    }
+}
---
+++ Code/Synnotech.FluentProcesses/ProcessBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Light.GuardClauses;
+
+namespace Synnotech.FluentProcesses;
+
+public sealed class ProcessBuilder
+{
+    public ProcessBuilder() : this(new ()) { }
+    
+    public ProcessBuilder(ProcessStartInfo processStartInfo)
+    {
+        ProcessStartInfo = processStartInfo.MustNotBeNull();
+    }
+    
+    private ProcessStartInfo ProcessStartInfo { get; }
+    private bool WasEnvironmentVariableSet { get; set; }
+    
+    public ProcessBuilder AddEnvironmentVariable(string name, string value)
+    {
+        ProcessStartInfo.EnvironmentVariables.Add(name, value);
+        WasEnvironmentVariableSet = true;
+        return this;
+    }
+
+    public ProcessBuilder Clone()
+    {
+        var processStartInfoClone = ProcessStartInfo.Clone(WasEnvironmentVariableSet);
+        return new (processStartInfoClone);
+    }
+
+    public Process CreateProcess()
+    {
+        var process = new Process { StartInfo = ProcessStartInfo };
+        // TODO: we need to set additional stuff here
+        return process;
+    }
+}
